38be0f25dee14208bba0b5be7d169173423ab4c6
Evan Loiterman <evanloiterman@gmail.com>
2021-11-01 13:59:15 -0500

    articles detail

--- src/app/article/article.page.ts
+++ src/app/article/article.page.ts
@@ -1,19 +1,3 @@
-/*import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'app-article',
-  templateUrl: './article.page.html',
-  styleUrls: ['./article.page.scss'],
-})
-export class ArticlePage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}*/
-
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router} from '@angular/router';
 import { ViewChild } from '@angular/core';
@@ -28,8 +12,9 @@ import { Article } from '../article.model';
   styleUrls: ['./article.page.scss']
 })
 export class ArticlePage implements OnInit {
-  article:Article = new Article();
-  articles: Article;
+  //article:Article = new Article();
+  //articles: Article;
+  article: Article;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -38,27 +23,26 @@ export class ArticlePage implements OnInit {
   ) { }
 //DEVENOTE check this against user page if needed
 
-//as used in ng-users:
-  /*ngOnInit() {
-    const id = this.route.snapshot.paramMap.get('id');
-    this.getArticle(id);
-  }*/
-//syntax from  users in ionix:
 
 
 ngOnInit() {    
   this.activatedRoute.params.subscribe(params=>{
-    this.getArticles();
+   
+   this.getArticle(params['articleId']);
  });
 }
 
-  getArticles(): void {
-    this.articlesService.getArticles().subscribe(
-      (response:any) => {
-        this.articles= response.articles;
-      }
-    );
-  }
+getArticle(id:string):void {
+ this.articlesService.getArticle(id).subscribe(
+   (response:any)=>{
+     // console.log(response);
+     this.article = response.article;
+   }
+ );
+}
+
+
+
 //DEVNOTE ADD LATER
   //3. Implement the deleteUser() method
   /*deleteArticle(id: string): void {
--- src/app/articles.service.ts
+++ src/app/articles.service.ts
@@ -13,8 +13,20 @@ const httpOptions = {
 
 
 export class ArticlesService {
-  private url: string = 'http://localhost:3000/api/articles';
-  constructor(private http: HttpClient) { }
+  //private url: string = 'http://localhost:3000/api/articles';
+  private url: string;
+  constructor(private http: HttpClient) {
+    let l = window.location;
+    let host:string;
+    //Are we running under Ionic or in a production environment?
+    if(l.port >= '8100'){
+      host = 'localhost:3000';
+    }else{
+      host = l.hostname + ((l.port.length>0)?':' + l.port:'');
+    }
+
+    this.url = `${l.protocol}//${host}/api/articles/`;
+  }
 
   getArticles(): Observable<Article[]> {
     return this.http.get<Article[]>(this.url);
